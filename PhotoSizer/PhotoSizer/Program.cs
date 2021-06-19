c6a23db1a86c14f5b5b0101a0ee4791ba5c95edd
William Denton <william.denton@pushpay.com>
2021-06-19 12:38:06 +1200

    add code

---
+++ PhotoSizer/PhotoSizer/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace PhotoSizer
+{
+    class Program
+    {
+        const string PhotoLibraryOriginals = "/Users/williamdenton/Pictures/iCloud Photo Library.photoslibrary/originals";
+        static void Main(string[] args)
+        {
+            var movies = new DirectoryInfo(PhotoLibraryOriginals)
+                .GetDirectories()
+                .SelectMany(s => s.GetFiles("*.mov"))
+                .ToList();
+
+            if (!Directory.Exists("media"))
+            {
+                Directory.CreateDirectory("media");
+            }
+
+            long inputBytes = 0;
+            long outputBytes = 0;
+
+            foreach (var sourceFile in (movies.OrderByDescending(f => f.Length).Take(100)))
+            {
+                var destFile = $"media/{Path.GetFileNameWithoutExtension(sourceFile.Name)}-compressed.mp4";
+
+                var psEncode = new ProcessStartInfo
+                {
+                    FileName = "./HandBrakeCLI",
+                    Arguments = $"--preset \"Fast 720p30\" --input \"{sourceFile.FullName}\" --output \"{destFile}\""
+                };
+                using var encode = Process.Start(psEncode);
+                encode.WaitForExit();
+
+                var psExif = new ProcessStartInfo
+                {
+                    FileName = "exiftool",
+                    Arguments = $"-TagsFromFile \"{sourceFile.FullName}\" -all:all \"{destFile}\" -overwrite_original"
+                };
+                using var exif = Process.Start(psExif);
+
+                exif.WaitForExit();
+
+                var outputFile = new FileInfo(destFile);
+
+                inputBytes += sourceFile.Length;
+                outputBytes += outputFile.Length;
+
+                Console.WriteLine($"Input {inputBytes / 1024 / 1024:0000.0}mb. Output {outputBytes / 1024 / 1024:0000.0}mb. Saved {(inputBytes - outputBytes) / 1024 / 1024:0000.0}mb");
+
+            }
+
+            Console.WriteLine($"Total=======================");
+            Console.WriteLine($"Input  {inputBytes / 1024 / 1024:0000.0}mb");
+            Console.WriteLine($"Output {outputBytes / 1024 / 1024:0000.0}mb");
+            Console.WriteLine($"Saved  {(inputBytes - outputBytes) / 1024 / 1024:0000.0}mb");
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # macos-photo-movie-compressor
-tool to quickly fid large `mov` files in the photo library and compress them, preserving metadata.
+tool to quickly find large `mov` files in the photo library and compress them, preserving metadata.
+
+I found i was running out of iCloud storage and didn't want to make the jump from 200Gb to 2Tb. I have lots of movies from my phone in my library and these were often filmed at 1080p60 which is excessive for what i need. It's just random clips of my kids.
+
+Editing files in photos in macos only makes ghe library bigger as it keeps all originals, so even where i had cropped the files down the original files were still there - and often multi-gigabyte.
+
+I considered making a shell script or automator action to do this, but it was easier for me t ojust write a few lines of code extract the movies, compress them, copy the meta data across then manuall add them back to the library, and then deleting the originals by hand.
+
+I did the top 100 movies in the library and recovered about 50Gb of space, which will hopefully last me a few more years before im forced to pay apple more money.
+
+## Prerequisites
+* Handbrake CLI https://handbrake.fr/downloads2.php
+* Exif Tool https://exiftool.org/
+* Grant the dotnet process permission to access all files in the file system (else you get permission exceptions)
+
+# Manual Steps
+1. run the dotnet app to collect all the movies and compress them
+1. add all the files back to photos
+1. from the recently imported folder in photos create an album of all the imported videos
+1. using the album, right click each video and `show in all photos`
+1. as the meta data is preserved the new and old videos should be side by side
+1. delete the old video
+1. back to the album, delete the video from there
+1. repeat until the album folder is empty
+
+I did the top 100 videos and this manual process took a while, but i dont see how it could be automated.
+
+# To be improved
+The original file name is lost as all files inside the phot library are just guids. That means when the file is reimported you'll loose any additional comments or names you had applied. Though its not hard to copy it across as you manuall process each file.
+
